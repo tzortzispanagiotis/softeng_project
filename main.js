f8e034f6e301b17a8de05f9767dfdc2c869a5884
Panagiotis Tzortzis <TzortzisPanagiotis@gmail.com>
2018-10-23 20:19:54 +0300

    add main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const express = require('express'),
       app     = express();
 
-
 app.get('/', (req, res) => {
   res.send("Hello World!");
 })
-
 
 app.listen(process.env.PORT || 1245, () => {
   console.log("Hello World");
