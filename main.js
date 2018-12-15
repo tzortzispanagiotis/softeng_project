a192f0f35c98d7bb24fa48bec1a954690ba6202b
unknown <tzortzispanagiotis@gmail.com>
2018-12-15 16:21:53 +0200

    started organising filesystem

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const express = require('express'),
       app     = express();
 
+
+//  set up a static folder. if the frontend asks for something starting with ./static on path, node will search 
+//  for it in public folder
+app.use("/static", express.static("./public"))
+
+// set view engine as ejs to omit .ejs when rendering a view
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
   res.send("Hello World!");
 })
@@ -9,4 +17,3 @@ app.listen(process.env.PORT || 1245, () => {
   console.log("Hello World");
 })
 
-a
