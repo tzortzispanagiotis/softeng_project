765973c000732af90119aacc2c530344eb5d9bc3
tzortzispanagiotis <tzortzispanagiotis@gmail.com>
2018-12-30 16:49:50 +0200

    added bodyParser

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 const express     = require('express'),
       app         = express(),
-      sequelize   = require("./database/connect");  // to create database connection
-      sample      = require("./database/sampletable.js"),
-
-
-//  set up a static folder. if the frontend asks for something starting with ./static on path, node will search
-//  for it in public folder
-app.use("/static", express.static("./public"))
+      sequelize   = require("./database/connect"),  // to create database connection
+      bodyParser  = require("body-parser"),
+      sample      = require("./database/sampletable.js");
 
 // set view engine as ejs to omit .ejs when rendering a view
 app.set("view engine", "ejs");
 
+//-------MIDDLEWARES-------//
+// set up a static folder. if the frontend asks for something starting with ./static on path,
+// node will search for it in public folder
+app.use("/static", express.static("./public"))
+
+// bodyParser for parsing POST requests:
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//------ROUTING------------//
+// API Router:
 const masterApiRouter = require("./routing/masterApiRouter.js")
 app.use("/observatory/api", masterApiRouter)
+// TODO: Index Routers
 
-app.listen(process.env.PORT || 1245, () => {
+
+
+// Initialize the server
+app.listen(process.env.PORT || 8765, () => {
   console.log("Hello World");
 })
