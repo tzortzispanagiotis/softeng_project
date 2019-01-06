d22210f19f6f18148587a216369a477e90e27593
dafne <dafne4696@gmail.com>
2019-01-06 15:11:35 +0200

    rest api for prices as described at the doc

--- controllers/API/pricesApiController.js
+++ controllers/API/pricesApiController.js
@@ -5,16 +5,48 @@ pricesApiController.getAllAction = (req, res) => {
     var params = {
         start: parseInt(req.query.start) || 0,
         count: parseInt(req.query.count) || 20,
-        //status: req.query.status || 'ACTIVE',
+        geoDist: req.query.geoDist ,
+        geoLng :req.query.geoLng ,
+        geoLat : req.query.geoLat , 
+        dateFrom : req.query.dateFrom , 
+        dateTo : req.query.dateTo , 
+        shops : req.query.shopIds , 
+        products : req.query.productIds , 
+        tags : req.query.tags , 
         sort: req.query.sort || 'id|DESC'
+    }
+    if ((params.geoDist==null) && (!(params.geoLat==null) || !(params.geoLng==null))) {
+        res.status(400).json({error: 'You have to fill in either  all the distance related boxes or none'})
+
+    }
+    if ((params.geoLat==null) && (!(params.geoDist==null) || !(params.geoLng==null))) {
+        res.status(400).json({error: 'You have to fill in either all the distance related boxes or none'})
+
+    }
+    if ((params.geoLng==null) && (!(params.geoLat==null) || !(params.geoDist==null))) {
+        res.status(400).json({error: 'You have to fill in either all the distance related boxes or none'})
+    
+    }
+    if ((params.dateFrom==null) && (!(params.dateTo==null))){
+        res.status(400).json({error: 'You have to fill in either all the DATE related boxes or none'})
+
+    }
+    if ((params.dateTo==null) && (!(params.dateFrom==null))) {
+        res.status(400).json({error: 'You have to fill in either all the DATE related boxes or none'})
+
+    }
+    if ((dateFrom==null) && (dateTo==null)) {
+         params.dateFrom =  sequelize.now, //sequelize.literal('CURRENT_TIMESTAMP') ; 
+         params.dateTo = sequelize.now//sequelize.literal('CURRENT_TIMESTAMP');
     }
     findallparam = {
         start:params.start ,
         count:params.count }
 
-db.Price.findAll(findallparam).then(foundPrices => {
+db.Price.findAndCountAll({where : { shopId:  {[Op.in]: params.shops} , productId: {[Op.in]: params.products , db.Shop } ,offset : params.start , limit :params.count}).then(found => {
     var prices= [];
     var total = 0;
+    foundPrices = found.rows
     foundPrices.forEach(foundPrice => {
         prices.push ({
             id: foundPrice.priceId,
