5023c20f4531f6bf21c9ac3b27510b35076f6756
STamatopoulos Efstathios <stathis@gmail.com>
2019-02-27 16:28:39 +0200

    navbar is working. login is migrated to bootstrap 4. added bs4 cdn

--- public/js/loggedin.js
+++ public/js/loggedin.js
@@ -1,41 +1,41 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+  console.log( "ready!" );
 });
 
 
 function show_loggedin(){
-  if (localStorage.token!=null){
-    $("#list").prepend('<li id="loggedin" title="Δες το προφιλ σου"><a href="">'+localStorage.username+'</a></li>');
-    $("#login").html('<a href="">Αποσυνδεση</a>').attr("name","logout");
-  }
-  else {
-    $("#login").html('<a href="">Συνδεσου</a>').attr("name","login");
-    $("#loggedin").remove();
-  }
+if (localStorage.token!=null){
+  $("#list").prepend('<li class="nav-item" id="loggedin" title="Δες το προφιλ σου"><a class="nav-link" href="">'+localStorage.username+'</a></li>');
+  $("#login").html('<a class="nav-link" href="">Δες το προφίλ σου</a>').attr("name","logout");
+}
+else {
+  $("#login").html('<a class="nav-link" href="">Συνδέσου</a>').attr("name","login");
+  $("#loggedin").remove();
+}
 }
 
 show_loggedin();
 
 $("#login").click(function (event,status){
-      event.preventDefault();
-      myData = {
-          token: localStorage.getItem("token")
-      }
-      if ($(this).attr("name")=="logout") {
-        $.ajax({
-            url: "/observatory/api/logout",
-            method: "POST",
-            data: myData,
-            success: function(data,status) {
-                localStorage.removeItem("token")
-                localStorage.removeItem("username")
-                alert("Logout successful. Redirect to main page.")
-                show_loggedin()
-                $( location ).attr("href", "/");
-            }
-        })
-      }
-    else {
-      $( location ).attr("href", "/login");
+    event.preventDefault();
+    myData = {
+        token: localStorage.getItem("token")
     }
+    if ($(this).attr("name")=="logout") {
+      $.ajax({
+          url: "/observatory/api/logout",
+          method: "POST",
+          data: myData,
+          success: function(data,status) {
+              localStorage.removeItem("token")
+              localStorage.removeItem("username")
+              alert("Logout successful. Redirect to main page.")
+              show_loggedin()
+              $( location ).attr("href", "/");
+          }
+      })
+    }
+  else {
+    $( location ).attr("href", "/login");
+  }
 })
