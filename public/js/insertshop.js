24b3ca83d6b93000b03dfb4e3348c864037d6d18
Tzortzis Panagiotis <tzortzispanagiotis@gmail.com>
2019-03-01 20:16:54 +0200

    Added functionality on shop insert

---
+++ public/js/insertshop.js
@@ -0,0 +1,65 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+  });
+  
+$("#submit-btn").click(function(event) {
+    event.preventDefault();
+
+    var inputData = {
+        name : $("#shop-name").val(),
+        address : $("#shop-address").val()
+    }
+
+    var temp = inputData.address.split(" ")
+
+    var list = ""
+    for (var i in temp) {
+        list = list + temp[i]
+        list = list + "+"
+    }
+    list = list.substring(0, list.length - 1)
+
+
+    var geocode = {}
+    $.ajax({
+        url: "https://maps.googleapis.com/maps/api/geocode/json?address="+list+"&key=AIzaSyCL-EHbgBsjgKSaP4hZ38JFx2GtLv9R5wM",
+        method: "GET",
+        success: function(data,status) {
+            geocode.lat = data.results[0].geometry.location.lat
+            geocode.lng = data.results[0].geometry.location.lng
+        },
+        error: function(data,status) {
+            alert("PULO")
+        }
+    })
+
+    console.log(geocode) // HERE WE HAVE lat, lng!!!
+
+    createData = {
+        name : inputData.name,
+        address : inputData.address,
+        longtitude : geocode.lng,
+        latitude : geocode.lat,
+        tags : "a"
+    }
+
+    $.ajax({
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        },
+        url: "/observatory/api/shops",
+        method: "POST",
+        data: createData,
+        
+        success: function(data,status) {
+            alert("Το κατάστημα προστέθηκε επιτυχώς!")
+            $( location ).attr("href", "/");
+        },
+        error: function(data,status) {
+            alert("Αποτυχία. Προσπαθήστε ξανά")
+            $( location ).attr("href", "/insertshop");
+        }
+    })
+
+})
+  
