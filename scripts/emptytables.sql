80699ad17be394aba01da6bdf35198e6b6894b76
angelospl48 <angelospl48@gmail.com>
2019-03-05 19:33:56 +0200

    otileeiotitlos

---
+++ scripts/emptytables.sql
@@ -0,0 +1,12 @@
+use rns_softeng_database;
+delete from prices;
+delete from shop;
+delete from product;
+delete from users;
+
+
+
+
+/*trexoume tin parakatw entoli
+mysql -umy_user -p < ./scripts/emptytables.sql
+*/
